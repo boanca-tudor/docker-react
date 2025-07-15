afa30bf8139b5651e4fe97f74d95891d1e70b352
tudor <tudorboanca@gmail.com>
2025-07-15 15:34:58 +0300

    Initial commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Bagameas gura ta!
         </p>
         <a
           className="App-link"
